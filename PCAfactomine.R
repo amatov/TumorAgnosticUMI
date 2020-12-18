14f4cd05bb6058840a3c8b83a4eb09985078a898
Alex  Matov <matov@clin.au.dk>
2020-12-18 12:33:54 +0000

    Replace PCAfactomine.R

--- PCAfactomine.R
+++ PCAfactomine.R
@@ -48,8 +48,44 @@ res.pca = PCA(mafsQ2, scale.unit=TRUE, ncp=5, graph=T)
 dim(mafsQ2) #24 72376
 ########################################################
 cohort2 <- rbind(mafsP2, mafsQ2)
+cohort3 <- rbind(cohort2, mafsI2)
+cohort4 <- rbind(cohort3, mafsC2)
 dim(cohort2) # 69 72376
-res.pca = PCA(cohort2, scale.unit=TRUE, ncp=5, graph=T) 
+res.pca = PCA(cohort2, scale.unit=TRUE, ncp=5, graph=T)
+dim(cohort3) # 283 72376
+res.pca = PCA(cohort3, scale.unit=TRUE, ncp=5, graph=T) 
+dim(cohort4) # 373 72376
+res.pca = PCA(cohort4, scale.unit=TRUE, ncp=5, graph=T) 
+### IMPROVE #####
+pileupsI <- list.files("~/genomedk/PolyA/faststorage/BACKUP/IMPROVE/sporacrc/N227", recursive = T, full.names = T, pattern = "bait.pileup$")
+pon_hg19 <- readRDS("~/genomedk/PolyA/faststorage/BACKUP/IMPROVE/call/references/200419_novaseq-xgen-sporacrc-pon.RDS") 
+countsI0 <-  piles_to_counts(files = pileupsI[1:214], regions = pon_hg19$regions)
+countsI1 <- countsI0[,,1:4] + countsI0[,,6:9]
+mafsI1 = array(0, dim=c(dim(countsI1)[1],dim(countsI1)[2],dim(countsI1)[3]))
+auxMI <- rowSums(countsI1, dims = 2) 
+for (i in 1:dim(countsI1)[1]) {
+  mafsI1[i,,] <- countsI1[i,,]/auxMI[i,]
+}
+mafsI2 = array(0, dim=c(dim(mafsI1)[1],dim(mafsI1)[2]*dim(mafsI1)[3]))
+for (i in 1:dim(countsI1)[1]) {
+  mafsI2[i,]  = mafsI1[i,,]
+}
+res.pca = PCA(mafsI2, scale.unit=TRUE, ncp=5, graph=T) 
+### CRUK #####
+pileupsC <- list.files("~/genomedk/PolyA/faststorage/BACKUP/CRUK/plasma/N289", recursive = T, full.names = T, pattern = "bait.pileup")
+pon_hg19 <- readRDS("~/genomedk/PolyA/faststorage/BACKUP/IMPROVE/call/references/200419_novaseq-xgen-sporacrc-pon.RDS") 
+countsC0 <-  piles_to_counts(files = pileupsC[1:90], regions = pon_hg19$regions)
+countsC1 <- countsC0[,,1:4] + countsC0[,,6:9]
+mafsC1 = array(0, dim=c(dim(countsC1)[1],dim(countsC1)[2],dim(countsC1)[3]))
+auxMC <- rowSums(countsC1, dims = 2) 
+for (i in 1:dim(countsC1)[1]) {
+  mafsC1[i,,] <- countsC1[i,,]/auxMC[i,]
+}
+mafsC2 = array(0, dim=c(dim(mafsC1)[1],dim(mafsC1)[2]*dim(mafsC1)[3]))
+for (i in 1:dim(countsC1)[1]) {
+  mafsC2[i,]  = mafsC1[i,,]
+}
+res.pca = PCA(mafsC2, scale.unit=TRUE, ncp=5, graph=T) 
 ###########################################################
 # Hierarchical cluster analysis on a set of dissimilarities and methods for analyzing it. 
 aux <- t(mafsQ2[,1:5])
