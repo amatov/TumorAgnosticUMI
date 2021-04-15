71daa6afa3a1eb52c2bb0da8a22a5ce8b871d2a6
Alex  Matov <matov@clin.au.dk>
2021-04-15 06:28:13 +0000

    Replace workflow.py

--- workflow.py
+++ workflow.py
@@ -1,12 +1,21 @@
 from gwf import Workflow, AnonymousTarget
 from os.path import join
 from glob import glob
-gwf = Workflow()
+#gwf = Workflow()
+gwf = Workflow(
+        defaults={
+            'cores': 1,
+            'memory': '16g',
+            'walltime': '01:00:00'})
+
+bam_files=glob('~/genomedk/matov/umiseq_analysis/CRUK5Mb/*/*consensus.sort.bam')
+
+print(bam_files)
 
 output_dir = "/genomedk/matov/umiseq_analysis/CRUK5Mb/"
-bams = glob("*.sorted.bam")
+
 bed_file = "NEW_METHOD_hg38_08feb2016_capture_targets.bed"
-for bam in bams:
+for bam in bam_files:
     bam_id = bam.split("/")[-1].split(".")[0]
     output = join(output_dir, f"{bam_id}.txt")
     gwf.target(
@@ -15,7 +24,7 @@ for bam in bams:
         outputs=[output],
         walltime="04:00:00",
     ) << """
-    python UMI_seq_fragment_length.py {} {} {}
+    python ~/genomedk/matov/umiseq_analysis/CRUK5Mb/UMI_seq_fragment_length.py {} {} {}
     """.format(
         bam, bed_file, output
     )
